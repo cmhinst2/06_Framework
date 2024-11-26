81ac158c3a90fbca25e4bdebcc69aaa54420cc7d
cmhinst2 <cmhinst2@gmail.com>
2024-11-26 09:53:21 +0900

    스프링 예외처리

--- boardProject-boot/src/main/java/edu/kh/project/member/model/mapper/MemberMapper.java
+++ boardProject-boot/src/main/java/edu/kh/project/member/model/mapper/MemberMapper.java
@@ -14,7 +14,7 @@ public interface MemberMapper {
 	 * @param memberEmail
 	 * @return loginMember
 	 */
-	Member login(String memberEmail);
+	Member login(String memberEmail) throws Exception;
 
 	/** 이메일 중복검사
 	 * @param memberEmail
